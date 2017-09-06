551223850a32e002fd64618fc56bcbbeba1c68b3
Vladislav Korbut <v.korbut8@gmail.com>
2017-09-06 18:23:57 +0300

    visual improvements

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Panel } from 'react-bootstrap';
+import moment from 'moment';
 
 function Message(props) {
   return (
-    <Panel
-      header={props.message.user.name}
-      footer={<div>{props.message.time}</div>}
-    >
-      {props.message.text}
-    </Panel>
+    <div className="message-container clearfix">
+      <div className={`message${props.userId === props.message.user._id ? ' message--my' : ''}`}>
+        <div className="message__header">
+          <span className="message__username">{props.message.user.name}</span>
+          <span className="message__time">{moment(props.message.time).format('DD.MM HH:mm')}</span>
+        </div>
+        <div className="message__body">
+          {props.message.text}
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FormControl, InputGroup, Button } from 'react-bootstrap';
 import { Scrollbars } from 'react-custom-scrollbars';
 import Fa from 'react-fontawesome';
@@ -10,16 +11,15 @@ class Chat extends Component {
     super();
     this.state = {
       message: '',
-      messages: [],
+      messages: [{ "_id": "59b00312d9350800e85e519a", "text": "1231321312", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707346000 }, { "_id": "59b00314d9350800e85e519b", "text": "32131321321выпа", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707348000 }, { "_id": "59b00315d9350800e85e519c", "text": "вавы", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707349000 }, { "_id": "59b00316d9350800e85e519d", "text": "ывавыа", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707350000 }, { "_id": "59b00317d9350800e85e519e", "text": "ываываываыва", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707351000 }, { "_id": "59b00319d9350800e85e519f", "text": "ывыаыаыва", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707353000 }, { "_id": "59b0031bd9350800e85e51a0", "text": "привет", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707355000 }, { "_id": "59b0031cd9350800e85e51a1", "text": "чошлвоа", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707356000 }, { "_id": "59b0031ed9350800e85e51a2", "text": "шгвшаг", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707358000 }, { "_id": "59b0031fd9350800e85e51a3", "text": "шгшгшг", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707359000 }, { "_id": "59b00321d9350800e85e51a4", "text": "гшгшг", "user": { "_id": "59ace828b54962229ce81bb8", "name": "789" }, "time": 1504707361000 }],
     };
   }
 
   componentDidMount() {
     socket.on('message', (message) => {
-      console.log(message);
       this.setState({
         message: '',
-        messages: [message, ...this.state.messages],
+        messages: [...this.state.messages, message],
       });
     });
   }
@@ -42,9 +42,25 @@ class Chat extends Component {
   render() {
     return (
       <div>
-        <Scrollbars style={{ position: 'absolute', left: 0, height: '80%' }} onUpdate={this.scrollToBottom}>
+        <Scrollbars
+          className="messages-container"
+          style={{
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            paddingTop: 90,
+            paddingBottom: 50,
+          }}
+          onUpdate={Scrollbars.scrollToBottom}
+        >
           <div className="container">
-            {this.state.messages.map(message => <Message key={message._id} message={message} />)}
+            {
+              this.state.messages.map(message => (<Message
+                key={message._id}
+                message={message}
+                userId={this.props.user.id}
+              />))
+            }
           </div>
         </Scrollbars>
         <div className="message-input">
@@ -68,4 +84,8 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+const mapStateToProps = (store) => ({
+  user: store.userReducer,
+});
+
+export default connect(mapStateToProps)(Chat);
